9b03ad9ee957d017bd9a38a7b7a206db2a09381e
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-13 16:46:23 +0530

    setup the AuthGuard

---
+++ src/auth/AuthGurd.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import useAuth from '../hooks/useAuth.hook';
+import AuthSpinner from '../components/general/AuthSpinner';
+import { PATH_PUBLIC } from '../routes/paths';
+
+
+const AuthGuard = ({ roles }) => {
+    const { isAuthenticated, user, isAuthLoading } = useAuth();
+
+    // Do we have access to the requeted page(the page will be rendered in <Outlet />)
+    const hasAccess = isAuthenticated && user?.roles?.find((q) => roles.includes(q));
+
+    if(isAuthLoading){
+        return <AuthSpinner/>;
+    }
+
+    return hasAccess ? <Outlet /> : <Navigate to={PATH_PUBLIC.unauthorized} />;
+};
+
+export default AuthGuard;
